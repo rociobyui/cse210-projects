59f44fed1721c812a93874339f70dc5a2d760c39
rociobyui <chiocedillo5@gmail.com>
2024-01-12 10:17:48 -0800

    Prep1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        // My code. 
+        //Console.WriteLine(Environment.NewLine + "Your name is " + last + "," + " " + first + " " + last + ".");
+        
+        //Using "$" to format the display. It is the right one.
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
