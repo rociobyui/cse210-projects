a8d9ae2d323b0c4bd3f40fa57dce1fbe42f16148
rociobyui <chiocedillo5@gmail.com>
2025-09-18 00:49:14 -0700

    Prep1 Assignment completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Using Console.Write() to prompt the user first name Python = print
+        Console.Write("What is your first name? ");
+        // Reading the user's first name
+        string first = Console.ReadLine();
+
+        //Using Console.Write() to prompt the user for last name in the same line
+        Console.Write("What is your last name? ");
+        // Reading and storing the user's last name in the string variable
+        string last = Console.ReadLine();
+
+        // Leaving a blank line
+        Console.WriteLine();
+
+        // Displaying or printing the user's full name, first only last and then full name.
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
